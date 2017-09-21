9213db08a12d632d8aa18b3a2d4802bcbd564b39
Alejandro Bravo <alebj88@gmail.com>
2017-09-20 20:50:49 -0400

    Argo Buoy ready

---
+++ Functions.R
@@ -0,0 +1,85 @@
+#Funciones empleadas
+
+#Plot de geolocalizacion de la raw data en archivos PNG
+MapaRawData<-function(df,file,title,lonIni=-66.715,latIni=13.115,zoom=6,size=2){
+    png(file=file)
+    map<-get_map(location= c(lon=lonIni,lat=latIni),zoom=zoom,maptype="terrain") 
+    plotMapa<-ggmap(map) +
+        geom_point(data=df,aes(x=lon,y=lat),col="red",show.legend=FALSE,size=size) +
+        labs(title=title)	
+    plot(plotMapa)
+    dev.off()
+}
+
+#Transformacion a formato decimal para geolocalizacion.
+geo2dec<-function(c){
+    z<-sapply(strsplit(c, "[o.\']"),as.character)
+    dec<- as.numeric(z[1, ]) + as.numeric(z[2, ])/60 + as.numeric(z[3, ])/3600
+    if (z[4, ]=="N"||z[4, ]=="E") {
+        dec 
+    }else{
+        -dec
+    } 
+}
+
+#Formateo de las filas de los archivos txt descargados de la NOAA
+formato<-function(fila,Tab){
+    vect<-strsplit(as.character(fila)[1],split="")[[1]]
+    if(Tab==TRUE){
+        vect<-gsub("\t"," ",vect)
+    }
+    s<-which(vect==" ")
+    pos<-rep(0,length(s))
+    for(i in 1:(length(s)-1)){
+        if(s[i]+1!=s[i+1]){
+            pos[i]<-1
+        }
+    }
+    pos[length(s)]<-1
+    if(any(pos==0)){
+        fila<-stringr::str_trim(paste0(vect[-s[pos==0]],collapse=""))
+    }else{
+        fila<-stringr::str_trim(paste0(vect,collapse=""))
+    }
+    return(strsplit(fila,split=" ")[[1]])
+}
+
+#Importe y formateo de los archivos txt descargados de la NOAA
+importar<-function(file,Tab=FALSE){
+    data <- read_csv(paste0("~/Thesis Project/Data/Raw Data/",file))
+    data <- as.data.frame(data)
+    k<-t(apply(data[-1,],1,formato,Tab))
+    data[2:nrow(data),]<-k
+    return(data)
+}
+
+#Construccion del dataframe con los datos de geolocalizacion
+dfGeo<-function(lista){
+    lat<-c(lista[[1]][1],lista[[2]][1],lista[[3]][1])
+    lon<-c(lista[[1]][2],lista[[2]][2],lista[[3]][2])
+    lat<-geo2dec(lat)
+    lon<-geo2dec(lon)
+    return(data.frame(lat=lat,lon=lon))
+}
+
+#Data frames con columnas de caracteres as.factor
+df.as.factor<-function(df){
+    pos<-which(unname(sapply(df,class))=="character")
+    if(length(pos)!=0){
+        for (i in 1:length(pos)){
+            df[,pos[i]]<-as.factor(df[,pos[i]])
+        }
+    }
+    return(df)
+}
+
+#Merge ffdf all=TRUE
+merge.ffdf.all<-function(x,y,by=intersect(names(x),names(y)),by.x=by,by.y=by){
+    aux1<-merge.ffdf(x,y,by.x,by.y,all.x=TRUE)
+    aux2<-merge.ffdf(y,x,by.x,by.y,all.x=TRUE)
+    aux2<-aux2[,names(aux1)]
+    aux2<-aux2[!(aux2$clave %in% aux1$clave),]
+    aux2<-as.ffdf(aux2)
+    df3<-ffdfappend(aux1,aux2)
+    return(df3)
+}

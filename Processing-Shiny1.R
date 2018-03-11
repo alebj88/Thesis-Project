a1bf04008ad925efb356aa07a47f0eb745040846
Alejandro Bravo <alebj88@gmail.com>
2018-03-11 16:49:00 -0400

    Add files via upload

---
+++ Processing-Shiny1.R
@@ -0,0 +1,39 @@
+#Creando archivos para la aplicacion Shiny-----------------------------------
+cat("Creando tablas para la aplicacion Shiny...\n")
+cat("\n")
+
+setwd("~/Thesis Project AB/R-Scripts")
+source('Functions.R')
+
+setwd("~/Thesis Project AB/Data/Pre-processed Data 2")
+
+#Datasets 
+DataCorales <- read.csv("DataCorales.csv")
+WOA <- read.csv("WOA.csv")
+WOA$clave2 <- NULL
+parques <- read.csv("parques.csv")
+
+#Toma lat lon de DataCorales y de WOA lat lon y la variable a imputar.
+#En dataF retorna DataCorales con la variable imputada.
+#Imputar variable por variable para mayor velocidad.
+#Si se emplea "median" la velocidad es apenas veces mayor.
+variables <- names(WOA)[!(names(WOA) %in% c("latitude","longitude"))]
+
+f_prod <- sapply(variables,function(v){
+    DataCorales <<- impute.per.region2(DataCorales,WOA,radio=c(5,10,65),
+                                       medida="mean",varName=v) 
+})
+
+setwd("~/Thesis Project AB/Data/Pre-processed Data")
+write.csv(DataCorales,file="shinyDataFactor-Var.csv",row.names=F)
+message("Se agrego a la carpeta 'Pre-processed Data' el archivo: 'shinyDataFactor-Var.csv'")
+
+#Limpiamos memoria
+rm(list=ls())
+gc()
+
+
+cat("Procedimiento Procesing-Shiny.R finalizado.\n")
+cat("\n")
+cat("/////////////////////////////////////////////////\n")
+cat("\n")
